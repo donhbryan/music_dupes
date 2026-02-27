026db0d64545867cbf1118e4e337e6f0129c4547
donhbryan <don.h.bryan@gmail.com>
2026-02-27 10:36:54 -0600

    Added the database to be tracked

---
+++ cleanup_empty_folders:.py
@@ -0,0 +1,18 @@
+import os
+
+def cleanup_empty_folders(folder):
+        """Recursively removes empty folders in the music dir."""
+        if not os.path.exists(folder):
+            return
+        print("Cleaning up empty source folders...")
+        for root, dirs, _ in os.walk(folder, topdown=False):
+            for name in dirs:
+                try:
+                    p = os.path.join(root, name)
+                    os.rmdir(p)  # Only removes if empty
+                except OSError:
+                    pass
+
+if __name__ == "__main__":
+    folder = "/mnt/ssk/unresolved"
+    cleanup_empty_folders(folder)
--- remove_junk_files.sh
+++ remove_junk_files.sh
@@ -1,1 +1,1 @@
-find /mnt/ssk/music -type f \( -name "*.jpg" -o -name "*.ini" -o -name "*.Identifier" \) -delete
+find /mnt/ssk/unresolved -type f \( -name "*.jpg" -o -name "*.ini" -o -name "*.Identifier" -o -name "*.db" -o -name "*.DS_Store" -o -name "*.IDX" \) -delete
