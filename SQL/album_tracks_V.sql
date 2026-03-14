b67fcf0da2b28ffe8f65031ea78c968e3b7c90a1
donhbryan <don.h.bryan@gmail.com>
2026-03-14 18:32:11 -0500

    update database view

--- SQL/album_tracks_V.sql
+++ SQL/album_tracks_V.sql
@@ -1,8 +1,7 @@
 DROP VIEW "main"."album_tracks_V";
 CREATE VIEW album_tracks_V as
-SELECT release_id, album_artist , album_title, substr('00'||track_no,-2,2)||' '||title as title, path 
+SELECT album_artist , album_title, substr('00'||track_no,-2,2)||' '||title as title, quality_score, path 
 from albums as a 
 INNER JOIN files as f
 on a.release_id = f.album_id
 order by 1 , 2 , 3
-commit;
